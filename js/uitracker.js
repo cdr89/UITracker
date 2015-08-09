5e2d3ad5430d093833c79bf92b70b254c1cf97c8
Domenico Rosario Caldesi <d.r.caldesi@gmail.com>
2015-08-09 19:27:28 +0200

    jQuery library for uitracker

---
+++ js/uitracker.js
@@ -0,0 +1,35 @@
+function UITracker(baseWebServiceURL, method) {
+    if (!baseWebServiceURL)
+        this.baseWebServiceURL = 'http://localhost/uitracker/webservices/trackevent.php';
+    else
+        this.baseWebServiceURL = baseWebServiceURL;
+    if (!method)
+        this.method = 'POST';
+    else
+        this.method = 'GET';
+
+    this.trackEvent = function (category, action, label, value, userId, userToken) {
+        var request = $.ajax({
+            url: this.baseWebServiceURL,
+            method: this.method,
+            data: {
+                category: category,
+                action: action,
+                label: label,
+                value: value,
+                userId: userId,
+                userToken: userToken
+            }
+        });
+        request.done(function (msg) {
+            // console.log(JSON.stringify(msg));
+        });
+        request.done(function (jqXHR, textStatus) {
+            // console.log(JSON.stringify(jqXHR));
+            // console.log(JSON.stringify(textStatus));
+        });
+    };
+}
+
+// Construct the instace of the tracker passing the webservice URL
+var UITracker = new UITracker(); //default instance
